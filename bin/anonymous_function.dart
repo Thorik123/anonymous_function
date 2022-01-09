a553a838dfc7a8efccca02a9a283f835a3fa6ee7
Thorik123 <tronikthorik07@gmail.com>
2022-01-09 17:45:51 +0700

    first commit

---
+++ bin/anonymous_function.dart
@@ -0,0 +1,9 @@
+void main(){
+  var besar = (String nama){
+    return nama.toUpperCase();
+  };
+
+var kecil = (String nama) => nama.toLowerCase();
+print(kecil('THORIK'));
+print(kecil('Tronik'));
+}
---
+++ test/anonymous_function_test.dart
@@ -0,0 +1,8 @@
+import 'package:anonymous_function/anonymous_function.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
